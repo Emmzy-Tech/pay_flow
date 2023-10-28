62eee89d565ecb305b7d0d96c8711785369d05eb
Franklin-tech01 <franklinmike2021@gmail.com>
2023-10-28 13:27:12 +0100

    removed unwanted comments

--- client/src/index.js
+++ client/src/index.js
@@ -11,18 +11,3 @@ root.render(
   </React.StrictMode>
 );
 
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import './index.css';
-// import App from './App';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <Router>
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>
-//   </Router>,
-//   document.getElementById('root')
-// );
