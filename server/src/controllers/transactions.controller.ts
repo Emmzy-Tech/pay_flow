754e9a755dc3168e362845658ed46c2c24371e4e
xlaez <utibeabasiekong2@gmail.com>
2023-11-09 13:20:21 +0100

    feat: add get transaction history to endpoints

--- server/src/controllers/transactions.controller.ts
+++ server/src/controllers/transactions.controller.ts
@@ -1,7 +1,7 @@
 import { configs } from '@/configs';
 import { AppServices } from '@/services/app';
 import { DolphControllerHandler, JWTAuthVerifyDec } from '@dolphjs/dolph/classes';
-import { Dolph, SuccessResponse, TryCatchAsyncDec } from '@dolphjs/dolph/common';
+import { Dolph, NotFoundException, SuccessResponse, TryCatchAsyncDec } from '@dolphjs/dolph/common';
 import { Request, Response } from 'express';
 
 const services = new AppServices();
@@ -20,5 +20,21 @@ export class TransactionController extends DolphControllerHandler<Dolph> {
     const paymentData = await services.transactionService.processPayment(payload.sub, req.body.employeeId, req.body.amount);
 
     SuccessResponse({ res, body: { data: paymentData, status: 'success', message: 'payment successful' } });
+  }
+
+  @TryCatchAsyncDec
+  @JWTAuthVerifyDec(configs.jwt.secret)
+  public async getTransactionHistory(req: Request, res: Response) {
+    //@ts-expect-error
+    const { payload } = req;
+
+    const transactions = await services.transactionService.getTransactionHistory(req.query?.keyword?.toString());
+
+    if (!transactions.length) throw new NotFoundException('there are no transactions yet');
+
+    SuccessResponse({
+      res,
+      body: { data: transactions, status: 'success', message: 'transaction history fetched successfully' },
+    });
   }
 }
