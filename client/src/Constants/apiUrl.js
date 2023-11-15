3af95d5ebb6b5ee370c0fd789f3c576eb7a041cf
xlaez <utibeabasiekong2@gmail.com>
2023-11-15 01:30:56 +0100

    update: add registerapi-connect basic register

---
+++ client/src/Constants/apiUrl.js
@@ -0,0 +1,2 @@
+export const devServer = "http://localhost:3300/v1";
+export const devSocketServer = "http://localhost:3000";
--- client/src/Pages/Register/index.jsx
+++ client/src/Pages/Register/index.jsx
@@ -2,59 +2,90 @@ import React from "react";
 import "../Login/login.css";
 // import googleLogo from '../../Assets/google.png'
 import { Link } from "react-router-dom";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { devServer } from "../../Constants/apiUrl";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const handleClick = () => {
-    navigate("/register/password")
-  }  
+  const handleClick = async (e) => {
+    e.preventDefault();
+    fetch(`${devServer}/auth/send-otp`, {
+      body: JSON.stringify({ email }),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.status === 401 || 400 || 500) {
+          setError(data.message);
+        } else if (data.status === 200) {
+          navigate("/register/password");
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  };
 
   return (
     <div className="login__container">
-    <div className="form__container">
-      <form action="" className="log">
-        <h2>Sign Up</h2>
-        <p>We are excited to have you join us</p>
-        {/* <button className="first__btn">
+      <div className="form__container">
+        <form action="" className="log">
+          <h2>Sign Up</h2>
+          <p>We are excited to have you join us</p>
+          {/* <button className="first__btn">
             <img src={googleLogo} alt="" srcset="" className="google-logo"/>
           Sign in with Google
         </button> */}
-        {/* <br /> */}
-        {/* <div className="line-container">
+          {/* <br /> */}
+          {/* <div className="line-container">
           <div className="line"></div>
           <span>or</span>
           <div className="line"></div>
         </div> */}
 
-        <div className="email__container">
-          <br />
-          <label htmlFor="email">Email Address</label>
+          <div className="email__container">
+            <br />
+            <label htmlFor="email">Email Address</label>
 
-          <input
-            type="email"
-            name="email"
-            id="user__email"
-            placeholder="Note: the email used should be a company or work mail"
-          />
-        </div>
-        <div className="agree">
-          <input type="checkbox" name="" id="" />
-          <p>
-            By checking this box you have agreed to our
-            <a href="/signup  " className="link"> Privacy Terms and Conditions</a>
+            <input
+              type="email"
+              name="email"
+              id="user__email"
+              placeholder="Note: the email used should be a company or work mail"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="agree">
+            <input type="checkbox" name="" id="" />
+            <p>
+              By checking this box you have agreed to our
+              <a href="/signup  " className="link">
+                {" "}
+                Privacy Terms and Conditions
+              </a>
+            </p>
+          </div>
+          <button className="second__btn" type="submit" onClick={handleClick}>
+            Sign Up
+          </button>
+
+          <p className="alt">
+            Have an account?{" "}
+            <Link to="/login" className="link">
+              Login here
+            </Link>
           </p>
-        </div>
-        <button className="second__btn" type="submit" onClick={handleClick}>Sign Up</button>
-
-        <p className="alt">
-          Have an account? <Link to="/login" className="link" >Login here</Link>
-        </p>
-      </form>
+        </form>
+      </div>
     </div>
-        </div>
   );
 };
 
