e7f857e2b408cbec8b69b3164de19935bdf71297
Emmzy-Tech <emmzytech@gmail.com>
2023-11-17 23:32:23 -0500

    Created Error Handler

---
+++ client/src/errorb.js
@@ -0,0 +1,29 @@
+// ErrorBoundary.js
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can log the error to an error reporting service here
+    console.error('Error caught by boundary:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render a fallback UI here
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; // Render children components normally
+  }
+}
+
+export default ErrorBoundary;
