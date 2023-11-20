7cf6662276770cf71ea0f1f58c2aeb818de8cd8e
xlaez <utibeabasiekong2@gmail.com>
2023-11-20 13:06:21 +0100

    update: switch from opay to squad

--- server/src/helpers/match_accout_code_to_name.helpers.ts
+++ server/src/helpers/match_accout_code_to_name.helpers.ts
@@ -1,4 +1,5 @@
 import banks from '@constants/opay_supported_banks.constants.json';
+import squadBanks from '@constants/squad_supported_banks.constants.json';
 
 interface JsonObject {
   code: string;
@@ -12,12 +13,20 @@ interface JsonObject {
   opayBank: string | null;
 }
 
-function findObjectByName(jsonArray: JsonObject[], name: string): JsonObject | null {
+interface SquadJsonObject {
+  id: number;
+  code: string;
+  name: string;
+  shortName: string;
+  shortCode: string;
+}
+
+function findObjectByName(jsonArray: SquadJsonObject[], name: string): SquadJsonObject | null {
   const result = jsonArray.find((obj) => obj.name === name);
   return result || null;
 }
 
 export const findBankByName = (name: string) => {
-  const result = findObjectByName(banks.data, name);
+  const result = findObjectByName(squadBanks.banks, name);
   return result;
 };
